56f6107401c1d4f0ef789b51d611dc4e069d4b42
mirandamon <nmiranda4@fordham.edu>
2016-03-29 19:58:59 -0400

    added facebook authentication

---
+++ Noted/Noted/Bridging-Header.h
@@ -0,0 +1,18 @@
+//
+//  Bridging-Header.h
+//  Noted
+//
+//  Created by Nathan Miranda on 3/29/16.
+//  Copyright © 2016 Miraen. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+
+
+#endif /* Bridging_Header_h */
