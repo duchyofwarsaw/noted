8d76c984e0dea5cf70f391922cb4a94411b125dc
elana tee <elanatee@gmail.com>
2016-04-01 22:48:14 -0400

    added baseViewController

---
+++ Noted/Noted/baseViewController.swift
@@ -0,0 +1,89 @@
+//
+//  baseViewController.swift
+//  Noted
+//
+//  Created by Elana Tee on 4/1/16.
+//  Copyright © 2016 Miraen. All rights reserved.
+//
+
+import UIKit
+
+class baseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.addSlideMenuButton()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func addSlideMenuButton(){
+        let btnShowMenu = UIButton(type: UIButtonType.System)
+        btnShowMenu.setImage(self.defaultMenuImage(), forState: UIControlState.Normal)
+        btnShowMenu.frame = CGRectMake(0, 0, 30, 30)
+        btnShowMenu.addTarget(self, action: "onSlideMenuButtonPressed:", forControlEvents: UIControlEvents.TouchUpInside)
+        let customBarItem = UIBarButtonItem(customView: btnShowMenu)
+        self.navigationItem.leftBarButtonItem = customBarItem;
+    }
+    
+    func defaultMenuImage() -> UIImage {
+        var defaultMenuImage = UIImage()
+        
+        struct Static {
+            static var onceToken: dispatch_once_t = 0
+        }
+        
+        dispatch_once(&Static.onceToken, { () -> Void in
+            UIGraphicsBeginImageContextWithOptions(CGSizeMake(30, 22), false, 0.0)
+            
+            UIColor.blackColor().setFill()
+            UIBezierPath(rect: CGRectMake(0, 3, 30, 1)).fill()
+            UIBezierPath(rect: CGRectMake(0, 10, 30, 1)).fill()
+            UIBezierPath(rect: CGRectMake(0, 17, 30, 1)).fill()
+            
+            UIColor.whiteColor().setFill()
+            UIBezierPath(rect: CGRectMake(0, 4, 30, 1)).fill()
+            UIBezierPath(rect: CGRectMake(0, 11,  30, 1)).fill()
+            UIBezierPath(rect: CGRectMake(0, 18, 30, 1)).fill()
+            
+            defaultMenuImage = UIGraphicsGetImageFromCurrentImageContext()
+            
+            UIGraphicsEndImageContext()
+        })
+        
+        return defaultMenuImage;
+    }
+    
+    // delegate (protocol) method call
+    func slideMenuItemSelectedAtIndex(index: Int32) {
+        let topViewController : UIViewController = self.navigationController!.topViewController!
+        print("View Controller is : \(topViewController) \n", terminator: "")
+        switch(index){
+        case 0:
+            print("Home\n", terminator: "") //Move to that view
+            break
+        case 1:
+            print("Play\n", terminator: "") //Move to that view
+            break
+        case 2:
+            print("Camera\n", terminator: "") //Move to that view
+            break
+        default:
+            print("default\n", terminator: "")
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
